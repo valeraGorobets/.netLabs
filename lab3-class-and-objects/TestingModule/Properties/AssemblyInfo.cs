b3b609100e622696c96d5e3a298fb92472a68a44
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-04 22:09:18 +0300

    Add test cases for BoolVector project

---
+++ lab3-class-and-objects/TestingModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestingModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestingModule")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14b9c84a-c986-4fb8-b78c-189dbd689f74")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab3-class-and-objects/TestingModule/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoolVectorLibrary;
+
+namespace TestingModule
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        BoolVector boolVector = new BoolVector(true, true, false, true, false, false, true);
+
+        [TestMethod]
+        public void TestInversion()
+        {
+            BoolVector invertedBoolVector = new BoolVector(false, false, true, false, true, true, false);
+            StringAssert.Equals(invertedBoolVector.GetVectorsStringFormat(), (!boolVector).GetVectorsStringFormat());
+        }
+
+        [TestMethod]
+        public void TestСonjunction()
+        {
+            BoolVector boolVector1 = new BoolVector(false, false, true, true);
+            BoolVector boolVector2 = new BoolVector(false, true, false, true);
+            BoolVector shouldBeEqualTo = new BoolVector(false, false, false, true);
+            StringAssert.Equals((boolVector1 & boolVector2).GetVectorsStringFormat(), shouldBeEqualTo.GetVectorsStringFormat());
+        }
+
+        [TestMethod]
+        public void TestDisjunction()
+        {
+            BoolVector boolVector1 = new BoolVector(false, false, true, true);
+            BoolVector boolVector2 = new BoolVector(false, true, false, true);
+            BoolVector shouldBeEqualTo = new BoolVector(false, true, true, true);
+            StringAssert.Equals((boolVector1 | boolVector2).GetVectorsStringFormat(), shouldBeEqualTo.GetVectorsStringFormat());
+        }
+
+        [TestMethod]
+        public void TestAmountOfZeroValues()
+        {
+            Assert.AreEqual(boolVector.CountAmountOf(FindValue.Zero), 3);
+        }
+
+        [TestMethod]
+        public void TestAmountOfOneValues()
+        {
+            Assert.AreEqual(boolVector.CountAmountOf(FindValue.One), 4);
+        }
+    }
+}
