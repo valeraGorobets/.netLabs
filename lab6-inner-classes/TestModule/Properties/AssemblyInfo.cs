af3b20c3aee70f352ba9d2c8fdb34a365c709918
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-06 23:11:14 +0300

    Implement tests to verify that Computer class works fine

---
+++ lab6-inner-classes/TestModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestModule")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e329026d-a3fa-49c0-84b0-84d034855730")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ lab6-inner-classes/TestModule/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6_inner_classes;
+
+namespace TestModule
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestOSNameSetter()
+        {
+            Computer windowsComputer = new Computer(Computer.OSName.Windows);
+            StringAssert.Equals(windowsComputer.operatingSystem, Computer.OSName.Windows);
+        }
+
+        [TestMethod]
+        public void TestRamAmountConfig()
+        {
+            Computer macComputer = new Computer(Computer.OSName.MacOS);
+            Console.WriteLine(macComputer.GetPreferableConfig());
+            Assert.AreEqual(macComputer.ram.Amount, 32);
+        }
+
+        [TestMethod]
+        public void TestRamTypeConfig()
+        {
+            Computer macComputer = new Computer(Computer.OSName.MacOS);
+            StringAssert.Equals(macComputer.ram.Type, "DDR4");
+        }
+
+        [TestMethod]
+        public void TestProcessorNameConfig()
+        {
+            Computer linuxComputer = new Computer(Computer.OSName.Linux);
+            StringAssert.Equals(linuxComputer.processor.Name, "Intel Core i5 7200U");
+        }
+
+        [TestMethod]
+        public void TestProcessorCoresConfig()
+        {
+            Computer linuxComputer = new Computer(Computer.OSName.Linux);
+            Assert.AreEqual(linuxComputer.processor.Cores, 2);
+        }
+
+        [TestMethod]
+        public void TestProcessorClockFrequencyConfig()
+        {
+            Computer otherComputer = new Computer(Computer.OSName.Other);
+            Assert.AreEqual(otherComputer.processor.ClockFrequency, 3);
+        }
+
+        [TestMethod]
+        public void TestWindowsComputerConfig()
+        {
+            const string windowsConfig = "OS Windows, 16GB of DDR4 onboard memory, Processor 3.8GHz 4 cores Intel Core i7 7700HQ";
+            Computer windowsComputer = new Computer(Computer.OSName.Windows);
+            StringAssert.Equals(windowsComputer.GetPreferableConfig(), windowsConfig);
+        }
+
+        [TestMethod]
+        public void TestMacComputerConfig()
+        {
+            const string macConfig = "OS MacOS, 32GB of DDR4 onboard memory, Processor 4.3GHz 6 cores Intel Core i9";
+            Computer macComputer = new Computer(Computer.OSName.MacOS);
+            StringAssert.Equals(macComputer.GetPreferableConfig(), macConfig);
+        }
+    }
+}
