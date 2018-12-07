b8a93a94c3ded483acab1226efd965b3af79e6ed
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-07 23:13:29 +0300

    Add base classes for application

---
+++ lab5-inheritance-and-polymorphism/lab5-inheritance-and-polymorphism/HouseUnit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lab5_inheritance_and_polymorphism
+{
+    public class Dimentions
+    {
+        public int height;
+        public int width;
+
+        public Dimentions(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        public override bool Equals(Object obj)
+        {
+            return obj is Dimentions dimentions &&
+                this.height == dimentions.height &&
+                this.width == dimentions.width;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.height.GetHashCode() ^ this.width.GetHashCode();
+        }
+    }
+    public abstract class HouseUnit
+    {
+        public Dimentions dimentions;
+        public int floor;
+
+        public abstract override bool Equals(object obj);
+        public abstract override int GetHashCode();
+        public abstract override string ToString();
+        public abstract void CloseOnKey();
+    }
+}
---
+++ lab5-inheritance-and-polymorphism/lab5-inheritance-and-polymorphism/Window.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab5_inheritance_and_polymorphism
+{
+    public class Window: HouseUnit
+    {
+        public string material;
+        private bool isWindowLocked = false;
+
+        public Window(
+            Dimentions dimentions,
+            int floor,
+            string material
+        ) {
+            this.dimentions = dimentions;
+            this.floor = floor;
+            this.material = material;
+        }
+
+        public override void CloseOnKey()
+        {
+            this.isWindowLocked = true;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return obj is Window window &&
+                 this.dimentions.Equals(window.dimentions) &&
+                 this.floor == window.floor &&
+                 this.material == window.material;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.dimentions.GetHashCode() ^ this.floor.GetHashCode() ^ this.material.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Window on {this.floor} floor, made of {this.material}: {this.dimentions.height} X {this.dimentions.width}";
+        }
+    }
+}
