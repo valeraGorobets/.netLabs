e435ec13fc716d07b83ea2704bae4b3aab7db7db
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-08 17:23:19 +0300

    Create base paper interface and abstract class Notebook

---
+++ lab7-interfaces/lab7-interfaces/BaseClasses.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab7_interfaces
+{
+    public interface IPaper<T>
+    {
+        void Burn(int temperature);
+        void WriteOnMe(T information);
+    }
+
+    public abstract class Notebook<T> : IPaper<T>
+    {
+        private readonly int IgnitionTemperature = 233;
+        private readonly int pageAmount;
+
+        public int PageAmount { get => PageAmount; set => PageAmount = value; }
+
+        public void Burn(int temperature)
+        {
+            if ( temperature >= this.IgnitionTemperature)
+            {
+                Console.WriteLine("Fire! Notebook is buring!");
+            }
+        }
+
+        public abstract void WriteOnMe(T information);
+    }
+}
