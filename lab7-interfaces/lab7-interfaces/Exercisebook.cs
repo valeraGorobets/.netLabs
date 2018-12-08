1473a97e6e4fafd3549c1815940ba53d2921031c
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-08 17:23:58 +0300

    Implement Exercisebook class

---
+++ lab7-interfaces/lab7-interfaces/Exercisebook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab7_interfaces
+{
+    public class Exercisebook<String>: Notebook<String>
+    {
+        public string subject;
+        public List<String> uniqueParagraphs = new List<String>();
+
+        public Exercisebook(int pageAmount, string subject)
+        {
+            this.PageAmount = pageAmount;
+            this.subject = subject;
+        }
+
+        public override void WriteOnMe(String text)
+        {
+            if (this.IsPlagiarismPassed(text)) {
+                uniqueParagraphs.Add(text);
+            } else
+            {
+                throw new Exception("Same text already exists in the Exercisebook!");
+            }
+        }
+
+        private bool IsPlagiarismPassed(String text)
+        {
+            if (this.uniqueParagraphs.Contains(text))
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
