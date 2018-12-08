c0d8fb90991d3a288d1ac67e510abfcf013d7e21
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2018-12-08 17:24:31 +0300

    Add DrawingNotebook class with Image entity

---
+++ lab7-interfaces/lab7-interfaces/DrawingNotebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab7_interfaces
+{
+    public class DrawingNotebook<Image> : Notebook<Image>
+    {
+        public List<Image> images = new List<Image>();
+        public DrawingNotebook(int pageAmount)
+        {
+            this.PageAmount = pageAmount;
+        }
+
+        public override void WriteOnMe(Image image)
+        {
+            this.images.Add(image);
+        }
+    }
+}
---
+++ lab7-interfaces/lab7-interfaces/Image.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab7_interfaces
+{
+    public struct Pixel
+    {
+        public int x;
+        public int y;
+        public string color;
+        public Pixel(int x, int y, string color)
+        {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
+
+    public class Image
+    {
+        public List<Pixel> pixels = new List<Pixel>();
+
+        public Image()
+        {
+
+        }
+        public void AddPixel(Pixel pixel)
+        {
+            this.pixels.Add(pixel);
+        }
+    }
+}
